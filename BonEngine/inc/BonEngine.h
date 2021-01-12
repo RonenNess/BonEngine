21588f6102857e2260e89db1eb1c19bec90d3c48
Ronen <RonenNess@gmail.com>
2021-01-12 22:32:29 +0200

    Update BonEngine.h

--- BonEngine/inc/BonEngine.h
+++ BonEngine/inc/BonEngine.h
@@ -14,8 +14,8 @@
 #include "Engine/Engine.h"
 #include "Engine/Scene.h"
 
-#define _BON_VERSION_STR "1.5.3"
-#define _BON_VERSION 1.53
+#define _BON_VERSION_STR "1.5.4"
+#define _BON_VERSION 1.54
 
 namespace bon
 {
